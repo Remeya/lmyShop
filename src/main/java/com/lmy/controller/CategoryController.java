bc0f096849eff0042f183263b7ad947dd507d299
LiMingYang <LiMingYang@newbee.com>
2019-05-06 18:22:39 +0800

    今天做了分页。明天做 添加

--- src/main/java/com/lmy/controller/CategoryController.java
+++ src/main/java/com/lmy/controller/CategoryController.java
@@ -1,21 +1,25 @@
 package com.lmy.controller;
 
+import com.lmy.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.lmy.pojo.Category;
 import com.lmy.service.CategoryService;
 
 import java.util.List;
-
+/*然后返回的是 Page4Navigator 类型，并通过 RestController 转换为 json 对象抛给浏览器。*/
 @RestController
 public class CategoryController {
     @Autowired
     CategoryService categoryService;
 
     @GetMapping("/categories")
-    public List<Category> list() throws Exception {
-        System.out.println("hh");
-        return categoryService.list();
+    public Page4Navigator<Category> list(@RequestParam(value = "start",defaultValue = "0")int start,@RequestParam(value = "size",defaultValue = "5")int size)
+            throws Exception {
+        start = start<0?0:start;
+        Page4Navigator<Category> page = categoryService.list(start,size,5);//5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
+        return page;
     }
 }
--- src/main/java/com/lmy/service/CategoryService.java
+++ src/main/java/com/lmy/service/CategoryService.java
@@ -3,7 +3,11 @@ package com.lmy.service;
 import com.lmy.dao.CategoryDAO;
 import com.lmy.pojo.Category;
 
+import com.lmy.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +18,13 @@ public class CategoryService {
     @Autowired
     CategoryDAO categoryDAO;
 
+    public Page4Navigator<Category> list(int start, int size, int navigatePages) {
+        Sort sort = new Sort(Sort.Direction.ASC, "id");
+        Pageable pageable = new PageRequest(start, size,sort);
+        Page pageFromJPA =categoryDAO.findAll(pageable);
+
+        return new Page4Navigator<>(pageFromJPA,navigatePages);
+    }
     public List<Category> list() {
         Sort sort = new Sort(Sort.Direction.DESC, "id");
         return categoryDAO.findAll(sort);
